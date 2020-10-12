f68969b884c1c55bf465d4b2ab7696ccc58911df
Drew M <drewmalette@protonmail.com>
2020-10-12 07:13:10 -0500

    revised scene object

---
+++ core/scene.py
@@ -0,0 +1,45 @@
+# scene.py:
+
+class Scene:
+
+    def __init__(self, fn): # fn = "filename"
+
+        self.uid = fn
+        
+        self.mobs = {}
+        self.live_mobs = {}
+        #self.buildings = {}
+        #self.furniture = {}
+        #self.loot = {}
+        #self.loot_count = 0
+        self.switches = {}
+        self.layerdata = { "bottom": None, "middle": None, "top": None, "collide": None }
+        self.tileset = None
+        
+        utilities.load_tmx(self.filename, self)
+        
+        self.paused = False # ?;
+        
+    #def add_loot(self, fn, x, y):    
+    #    uid = self.loot_count
+    #    px = x
+    #    py = y - 20
+        #self.loot[self.loot_count] = sprite.Loot(self, uid, fn, (px,py))
+        #self.loot_count = (self.loot_count + 1) % 256
+        
+    def add_mob(self, mob_obj):    
+        self.mobs[mob.name] = mob_obj
+    
+    def get_tile(self, layer, col, row):    
+        index = int((row % self.rows) * self.cols + (col % self.cols))
+        return self.layerdata[layer][index]
+            
+    def update(self, signals, fc):        
+        #if not self.game.fader.fading and not self.paused:
+        if not "fading" in signals and not self.paused:
+            for mob in self.live_mobs.values():
+                mob.update()
+            #self.game.camera.update()
+        
+    def render(self):    
+        self.game.camera.render()
